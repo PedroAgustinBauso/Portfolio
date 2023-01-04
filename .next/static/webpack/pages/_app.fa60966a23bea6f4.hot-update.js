d2c62adb20e5578e5d7a0ab480ff33bc52a16a07
Pedro <pedroagustinbauso@gmail.com>
2023-01-04 19:40:46 -0300

    Se agrego proyecto

---
+++ .next/static/webpack/pages/_app.fa60966a23bea6f4.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./src/data/data.json":
+/*!****************************!*\
+  !*** ./src/data/data.json ***!
+  \****************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"name":"Pedro Agustín Bauso","image":"/me.png","profession":"FullStack web developer","intro":"Focused on building products that will make people\'s lives better. I also create content in form of blogs and threads. I love to learn new things and I am always open to learn new technologies.","about":"Soy una persona que siempre hace mucha introspección. Suelo preguntar a mis amigos y conocidos diferentes puntos de vista sobre como veo situaciones de la vida, o propias actitudes mías. Siento que hacer esto me dio la habilidad de escuchar y estar abierto siempre al cambio, ya que considero que todas las personas estamos en constante crecimiento.","website":"https://www.linkedin.com/in/pedro-agustin-bauso/","skills":[{"src":"/logos/react.svg","name":"React","link":"https://reactjs.org/"},{"src":"/logos/Redux.png","name":"Redux","link":"https://redux-toolkit.js.org/"},{"src":"/logos/js.svg","name":"JavaScript","link":"https://developer.mozilla.org/es/docs/Web/JavaScript"},{"src":"/logos/css.png","name":"Css","link":"https://developer.mozilla.org/es/docs/Web/CSS"},{"src":"/logos/html.png","name":"Html","link":"https://developer.mozilla.org/es/docs/Web/HTML"},{"src":"/logos/node.svg","name":"NodeJs","link":"https://nodejs.org/en/"},{"src":"/logos/express.png","name":"Express","link":"https://expressjs.com/es/"},{"src":"/logos/postgre.png","name":"PostgreSQL","link":"https://www.postgresql.org/"},{"src":"/logos/sequelize.png","name":"Sequelize","link":"https://sequelize.org/"},{"src":"/logos/git.png","name":"Git","link":"https://git-scm.com/"},{"src":"/logos/github1.png","name":"GitHub","link":"https://github.com/"}],"projects":[{"name":"Gift You A Box","image":"/projects/ABox.png","about":"","tech":["React","Tailwind","JavaScript","Html","Css"],"links":{"GitHub":"https://github.com/PedroAgustinBauso/A-Box-Moment"},"role":"Front-end"},{"name":"Portfolio","image":"/projects/portfolioPrueba.png","about":"","tech":["React","Tailwind","Typescript"],"links":{"GitHub":"https://github.com/PedroAgustinBauso/Portfolio","Live":"https://gift-you-a-box.vercel.app/"},"role":"Front-end"},{"name":"E-commerce","image":"/projects/Cervezas.webp","about":"","tech":["React","Redux","Material UI","NodeJs","Express","PostgreSQL","Sequelize"],"links":{"GitHub":"https://github.com/PedroAgustinBauso/e-commerce","Live":"https://www.youtube.com/watch?v=8U38a3uWG5M&ab_channel=PedroBauso"},"role":"Full Stack Developer"},{"name":"Prode","image":"/projects/prodeTonic3.jpg","about":"","tech":["React","Redux","Tailwind","Material UI","NodeJs","Express","PostgreSQL","Sequelize"],"links":{"GitHub":"https://github.com/NazarenoRios/Tonic3-Prode"},"role":"Front-end"}],"testimonials":[{"value":"I have launched more than 2 products on Product Hunt with Avneesh and I have enjoyed the entire journey from getting the project idea till launching it on Product Hunt","name":"Kira","relation":"Co-worker","pfp":"https://avatars.githubusercontent.com/u/90365542?v=4","link":"https://github.com/Kira272921/"},{"value":"I\'ve worked on numerous projects with Avneesh and the flow has always been perfect. Avneesh is a great leader and we also listen to spotify together!","name":"Anurag","relation":"Co-builder","pfp":"https://avatars.githubusercontent.com/u/77309809?v=4","link":"https://anurag.tech"}],"links":[{"name":"Mail me","icon":"mail.svg","link":"mailto:pedroagustinbauso@gmail.com"},{"name":"GitHub","icon":"github.svg","link":"https://github.com/PedroAgustinBauso"},{"name":"LinkedIn","icon":"linkedin.svg","link":"https://www.linkedin.com/in/pedro-agustin-bauso/"}]}');
+
+/***/ })
+
+});
--- .next/static/webpack/webpack.c4b2d7a378cca3da.hot-update.js
+++ .next/static/webpack/webpack.c4b2d7a378cca3da.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "36a24de52257a6c2"; }
+/******/ 	__webpack_require__.h = function() { return "ad4a1a558fc23193"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
